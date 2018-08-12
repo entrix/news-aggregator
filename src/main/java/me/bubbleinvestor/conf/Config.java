662c069590f226a52cf4693284823b49b1cf6e1d
Олег Буршинов <mrzoom.214@gmail.com>
2018-08-12 16:09:58 +0300

    kibana add to docker compose

--- src/main/java/me/bubbleinvestor/conf/Config.java
+++ src/main/java/me/bubbleinvestor/conf/Config.java
@@ -3,8 +3,11 @@ package me.bubbleinvestor.conf;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.apache.http.HttpHost;
 
 @Configuration
 public class Config {
@@ -15,5 +18,18 @@ public class Config {
         objectMapper.findAndRegisterModules();
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
+    }
+
+    @Bean
+    RestHighLevelClient restHighLevelClient() {
+        return new RestHighLevelClient(
+                RestClient
+                        .builder(new HttpHost("elasticsearch", 9200))
+                        .setRequestConfigCallback(config -> config
+                                .setConnectTimeout(5_000)
+                                .setConnectionRequestTimeout(5_000)
+                                .setSocketTimeout(5_000)
+                        )
+                        .setMaxRetryTimeoutMillis(5_000));
     }
 }
